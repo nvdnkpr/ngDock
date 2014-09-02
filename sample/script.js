03ab020dfc94787d866497638cf920c4264cc13f
dardino <dardino@hotmail.com>
2014-09-02 09:43:27 +0200

    Update script.js

--- sample/script.js
+++ sample/script.js
@@ -1,2 +1,12 @@
-// Code goes here
+// Sample script to obtain dynamic dock 
 var dockapp = angular.module('testdock',['ngDock']);
+dockapp.controller('sampleController', function($scope) {
+  $scope.div2 = {};
+  $scope.div2.dock = 'left';
+  $scope.toggleLeftRight = function() {
+    if ($scope.div2.dock == 'left')
+      $scope.div2.dock = 'right';
+    else 
+      $scope.div2.dock = 'left';
+  };
+});
