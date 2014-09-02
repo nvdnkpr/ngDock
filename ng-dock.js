0eac8a8dbc26718b1785d77602c8e018342ed254
dardino <dardino@hotmail.com>
2014-09-02 09:41:45 +0200

    Update ng-dock.js
    added dynamic binding to dock info

--- ng-dock.js
+++ ng-dock.js
@@ -1,38 +1,76 @@
 angular.module('ngDock', [])
   .factory('dock', function() {
+    var valids = [ 'left', 'right', 'top', 'bottom', 'fill' ];
     return {
       dockRefs: {},
       Area: (function() {
-        var Area = function($div) {
+        function setPosition(r, s, pos) {
+            r.position = 'absolute';
+            if (pos != "bottom") r.top = s.top; else { r.top = 'auto'; }
+            if (pos != "left") r.right = s.right; else { r.right = 'auto'; }
+            if (pos != "right") r.left = s.left; else { r.left = 'auto'; }
+            if (pos != "top") r.bottom = s.bottom; else { r.bottom = 'auto'; }
+        }
+        function decreaseSize($element, pos, s) {
+            if ("top|bottom".indexOf(pos) != -1) size = $element.outerHeight(true);
+            if ("left|right".indexOf(pos) != -1) size = $element.outerWidth(true);
+            if (pos == "bottom") s.bottom += size;
+            if (pos == "left")   s.left   += size;
+            if (pos == "right")  s.right  += size;
+            if (pos == "top")    s.top    += size;
+        }
+        function removeClasses($element){
+          var x = valids.length;
+          while(x--)
+            $element.removeClass("dock-" + valids[x]);
+        }
+        function update($element, pos, s) {
+            var r = {};
+            removeClasses($element);
+            setPosition(r, s, pos);
+            $element.addClass("dock-" + pos);
+            $element.css(r);
+            decreaseSize($element, pos, s);
+        }
+        var Area = function($div, $scope) {
           var s = {
             top: 0,
             bottom: 0,
             left: 0,
             right: 0
           };
-          this.addChild = function($element, pos, size) {
-            var valids = [ 'left', 'right', 'top', 'bottom', 'fill' ];
+          var childList = [];
+          this.addChild = function($element, $pos) {
+            var pos = $scope.$eval($pos);
+          
             if (valids.indexOf(pos) == -1)
               throw {
                 message: "dock position must be one of this: " + valids.join(', ')
               };
-            var r = {};
-            r.position = 'absolute';
-            if (pos != "bottom") r.top = s.top;
-            if (pos != "left") r.right = s.right;
-            if (pos != "right") r.left = s.left;
-            if (pos != "top") r.bottom = s.bottom;
-            if ("top|bottom".indexOf(pos) != -1) r.height = size;
-            if ("left|right".indexOf(pos) != -1) r.width = size;
-            $element.addClass("dock-" + pos);
-            if ("top|bottom".indexOf(pos) != -1) size = $element.outerHeight(true);
-            if ("left|right".indexOf(pos) != -1) size = $element.outerWidth(true);
-            if (pos == "bottom") s.bottom += size;
-            if (pos == "left")   s.left   += size;
-            if (pos == "right")  s.right  += size;
-            if (pos == "top")    s.top   += size;
-            return r;
+            childList.push({
+              element: $element,
+              pos: $pos,
+              order: childList.length
+            });
+            update($element, pos, s);
           };
+          this.refresh = function() {
+            s.top = 0;
+            s.bottom = 0;
+            s.left = 0;
+            s.right= 0;
+            for (var x = 0; x < childList.length; x++) {
+              var pos = $scope.$eval(childList[x].pos);
+              update(childList[x].element, pos, s);
+            }
+          };
+          var _this = this;
+          $(window).resize(function() {
+            _this.refresh();
+          });
+          $scope.$watch(function() {
+            _this.refresh();
+          });
         };
         return Area;
       })()
@@ -41,11 +79,9 @@ angular.module('ngDock', [])
   .directive('dockRef', function(dock) {
     return {
       restrict: "A",
-      compile: function() {
-      },
       controller: function($scope, $element) {
         $scope.__dock_ref_id = String(Math.random()).replace(/\./g, "");
-        dock.dockRefs[$scope.__dock_ref_id] = new dock.Area($element);
+        dock.dockRefs[$scope.__dock_ref_id] = new dock.Area($element, $scope);
       },
       scope: true
     };
@@ -53,32 +89,12 @@ angular.module('ngDock', [])
   .directive('dock', function(dock) { 
     return {
       restrict: "A",
-      compile: function($element, $args) {
-        return { 
-          pre: function preLink($scope, $element, $args) {
-          },
-          post: function postLink($scope, $element, $args) {
-          }
-        };
-      },
       controller: function($scope, $element) {
-        var to = ['top', 'bottom', 'left', 'right', 'fill'];
-        if (to.indexOf($element.attr('dock').toLowerCase()) == -1) {
-          throw {
-            message: "dock position must be one of this: " + to.join(', ')
-          }
-        }
-        $scope.dock = {
-          position: $element.attr('dock').toLowerCase()
-        };
+        var dp = $element.attr('dock').toLowerCase();
         $scope.__dock_id = String(Math.random()).replace(/\./g, "");
-        var sz = 0;
-        if ("top|bottom".indexOf($scope.dock.position) != -1) sz = $element.outerHeight(true);
-        if ("left|right".indexOf($scope.dock.position) != -1) sz = $element.outerWidth(true);
         var area = dock.dockRefs[$scope.$parent.__dock_ref_id];
-        $element.css(area.addChild($element, $scope.dock.position, sz));
+        area.addChild($element, dp);
       },
       scope: true
     };
   })
-  
